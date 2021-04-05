12b9c04a0f83fff4614d826ee57d4ec64a7f2af9
ed <eogt04@gmail.com>
2021-04-05 09:24:40 -0500

    Update alpineexe.sh

--- alpineexe.sh
+++ alpineexe.sh
@@ -5,7 +5,7 @@ apk add curl
 apk add git
 apk add fzf
 
-curl https://gist.githubusercontent.com/Edwwarth/de795a4405f80ffa3b002de5581bda0d > ~/.vimrc
+curl https://raw.githubusercontent.com/Edwwarth/my-vimrc/main/.vimrc > ~/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
